422bde9915a02a378e72fa84d44a9dfeca5af6d2
Tim Sigl <tim.sigl@hotmail.de>
2019-04-12 14:50:43 +0200

    init commit

---
+++ public/index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+
+<html lang="de">
+    <head>
+        <meta charset="utf-8">
+        <title>Webprogrammierung Projekt</title>
+    </head>
+
+    <body>
+        <?php include ('header.php'); ?>
+            <section>
+                <h1>Main-Content</h1>
+            </section>   
+        <?php include ('footer.php'); ?>
+    </body>
+</html>
