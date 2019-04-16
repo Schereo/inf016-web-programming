14b49939765febe57e552a410485f416a0113cba
Nelly Klassen <nelly.klassen@uni-oldenburg.de>
2019-04-16 14:56:31 +0200

    navbar eingefügt

--- index.php
+++ index.php
@@ -23,9 +23,9 @@ Vix cu semper vivendo propriae.
 <p>Scripta mediocritatem nam eu, atomorum rationibus sententiae quo in. Elitr oblique est ex, vel iuvaret eripuit ad.
     Sea magna inimicus sadipscing in, per cetero fuisset ut. Error epicuri his te, an cibo duis regione has. Ad omnis
     melius fierent qui, est quas sonet latine ei, odio scaevola iudicabit qui id. Insolens necessitatibus ut pro.</p>
+<br> 
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2393.0944202097276!2d8.187361315219686!3d53.14439997993721!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47b6dfb1330a433f%3A0xcd64e0ccd2c6150b!2sBBS+Berufsbildende+Schulen+Haarentor!5e0!3m2!1sde!2sde!4v1555402008182!5m2!1sde!2sde" width="300" height="225" frameborder="0" style="border:0" ></iframe>
 <br>
-<img src="./media/karte.jpeg" width="550" height="382" alt="Bild2"> <br>
-
 <?php include 'pages/footer.php';?>
 </body>
 </html> 
--- pages/header.php
+++ pages/header.php
@@ -1,4 +1,13 @@
 <h1>Bildungsstätten</h1>
-<a href=>Home</a>
-<a href="pages/index.html">Stadtteile</a>
-<a href="https://google.de">Google</a>
+<nav>
+    <a href="pages/index.php">Home</a>
+    <a href="pages/index.html">Grundschulen</a>
+    <a href="pages/index.html">Oberschulen</a>
+    <a href="pages/index.html">Integrierte Gesamtschulen</a>
+    <a href="pages/index.html">Gymnasien</a>
+    <a href="pages/index.html">Förderschulen</a>
+    <a href="pages/BerufsbildendeSchulen.php">Berufsbildende Schulen</a>
+    <a href="https://google.de">Google</a>
+    <label for="search"></label>
+    <input type="search" id="search" placeholder="Suche…">
+</nav>
