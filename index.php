01aef55d3c76aced165646b96dde3b4928941ef6
Jens Schulte <jens.schulte@uni-oldenburg.de>
2019-04-29 14:51:29 +0200

    Layout innerhalb der Cards

--- index.php
+++ index.php
@@ -16,24 +16,16 @@
         <div class="strech-grid-item">
             <?php include 'pages/header.php';?>
         </div>
-        <div class="large-grid-item">
-            <header> 
-                <h1>Wilkommen auf unserer Webseite</h1>
-                <h2>Hier könnte auch Werbung für Ihre Grundschule stehen.</h2>
-                <p>Lorem ipsum dolor sit amet, eu tamquam appareat deterruisset pri, sea et vivendum suavitate mediocritatem. Qui puto
-                    essent option ex, pro in tractatos neglegentur. Omnium apeirian expetendis vel id.</p>
-                <p>Eirmod utamur vis in, nec eu ferri dicit. Eros sale libris ut ius.
-                    Vix cu semper vivendo propriae.</p>      
+        <div class="large-grid-item card">
+            <header >
                 <img src="/assets/images/oldenburg-stadt.jpeg" alt="Picture of Oldenburg" class="responsive">
-                <p>Scripta mediocritatem nam eu, atomorum rationibus sententiae quo in. Elitr oblique est ex, vel iuvaret eripuit ad.
-                    Sea magna inimicus sadipscing in, per cetero fuisset ut. Error epicuri his te, an cibo duis regione has. Ad omnis
-                    melius fierent qui, est quas sonet latine ei, odio scaevola iudicabit qui id. Insolens necessitatibus ut pro.</p>
             </header>
         </div>
         <div class="large-grid-item">
-            <section>
+            <section class="card">
+                <h2 id="#suche" class="card-header">Schule Suchen</h2>
+                <div class="card-body">
                 <form>
-                    <h2 id="#suche">Schule Suchen</h2>
                     <input type="text" placeholder="Schulname">
                     <select>
                         <option value="form1">Grundschule</option>
@@ -64,44 +56,58 @@
                     </select>
                     <button type="submit" name="anmelden">Suchen</button>
                 </form>
-                <?php include 'pages/detail.php';?>   
+                <?php include 'pages/detail.php';?>
+                </div>
             </section>
         </div>
         <div class="large-grid-item">
-            <section>
-                <h2 id="map">Map</h2>
+            <section class="card">
+                <h2 id="map" class="card-header">Map</h2>
+                <div class="card-body">
                 <iframe title="Map of Oldenburg" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2393.0944202097276!2d8.187361315219686!3d53.14439997993721!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47b6dfb1330a433f%3A0xcd64e0ccd2c6150b!2sBBS+Berufsbildende+Schulen+Haarentor!5e0!3m2!1sde!2sde!4v1555402008182!5m2!1sde!2sde" width="300" height="225" style="border:0" ></iframe>
                 <br>
+                </div>
             </section>
+
         </div>
         <div class="left-grid-item">
-            <section>
+            <section class="card">
+                <h2 id="anmelden" class="card-header">Anmelden</h2>
                 <form>
-                    <h2 id="anmelden">Anmelden</h2>
-                    <input type="text" name="mail" placeholder="E-Mail Adresse"><br>
-                    <input type= "password" name="passwort" placeholder="Passwort"><br>
+                    <div class="card-body">
+                        <input type="text" name="mail" placeholder="E-Mail Adresse"><br>
+                        <input type= "password" name="passwort" placeholder="Passwort"><br>
+                    </div>
+                    <div class="card-footer">
                     <button type="submit" name="anmelden">Anmelden</button>
-                    <br>
+                    <a>Passwort vergessen</a>
+                    </div>
                 </form>
             </section>
         </div>
         <div class="right-grid-item">
-            <section>
-                <form> 
-                    <h2 id=registrieren>Registrieren</h2>
-                    <input type= "text" name="firstName" placeholder="Vorname"><br>
-                    <input type="text" name="lastName" placeholder="Nachname"><br>
-                    <input type="email" name="email" placeholder="Email"><br>
-                    <input type="password" name="passwort" placeholder="Passwort"><br>
-                    <input type= "password" name="passwort2" placeholder="Passwort wiederholen"><br>
+            <section class="card">
+                <h2 id=registrieren class="card-header">Registrieren</h2>
+
+                <form>
+                    <div class="card-body">
+                        <input type= "text" name="firstName" placeholder="Vorname"><br>
+                        <input type="text" name="lastName" placeholder="Nachname"><br>
+                        <input type="email" name="email" placeholder="Email"><br>
+                        <input type="password" name="passwort" placeholder="Passwort"><br>
+                        <input type= "password" name="passwort2" placeholder="Passwort wiederholen"><br>
+                    </div>
+                    <div class = "registerbutton">
                     <button type="submit" name="registrieren" > Registrieren </button>
+                    </div>
                 </form>
             </section>
         </div>
         <div class="large-grid-item">
-            <section>
+            <section class="card">
+                <h2 id="anlegen" class="card-header">Anlegen</h2>
+                <div class="card-body">
                 <form>
-                    <h2 id="anlegen">Anlegen</h2>
                     <input type= "text" name="schulname" placeholder="Name der Schule">
                     <select>
                         <option value="form1">Grundschule</option>
@@ -131,11 +137,13 @@
                         <option value="st17">Wechloy</option>   
                     </select><br>
                     <textarea placeholder="Beschreibung"> </textarea>
-                    <br> 
-                    <input type="file" accept="image/png, image/jpeg">
                     <br>
+                    <div class="card-footer">
+                    <input type="file" accept="image/png, image/jpeg">
                     <button type="submit" name="anlegen">Schule Anlegen</button>
+                    </div>
                 </form>
+                </div>
             </section>
         </div>
         <div class="strech-grid-item">
