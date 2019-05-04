b84dcca90795707e994bd9ce6cd08848677f5ea1
Tim Sigl <tim.sigl@hotmail.de>
2019-05-04 12:25:35 +0200

    Resolved merge conflicts 2

--- pages/detail.php
+++ pages/detail.php
@@ -1,36 +1,3 @@
-<<<<<<< HEAD
-<h3> Berufsbildene Schulen</h3>
-<article>  
-    <h4> BBS Haarentor</h4>
-    <img src="../assets/images/bbs1.jpeg" alt="Picture of BBS Wechloy">   
-    <p> Berufsbildungszentrum für Wirtschaft, Informatik und Gesundheit<br>
-        <b>Schlleitung:</b> Diedrich Ahlfeld<br>
-        <b>Adresse:</b> Ammerländer Heerstraße 33/39, 26129 Oldenburg<br>
-        <b>Telefon:</b> 0441 77915-0<br>
-        <b>E-Mail:</b> <a href="mailto:mail@bbs-haarentor.de">mail@bbs-haarentor.de</a><br>
-        <b>Internet:</b> <a href="http://www.bbs-haarentor.de">www.bbs-haarentor.de</a><br>
-        <b>Sekretariat:</b> Montag bis Donnerstag von 7.30 Uhr bis 12 Uhr sowie von 12.30 Uhr
-        bis 15 Uhr, Freitag von 7.30 Uhr bis 13.15 Uhr</p>
-    <aside> 
-        <h5>Weitere Infos</h5>
-        <dl>
-            <dt>Schülerinnen/Schüler</dt>
-            <dd>circa 2.300</dd>
-            <dt>Mitarbeiterinnen/Mitarbeiter</dt>
-            <dd>circa 110</dd>
-        </dl>
-        Verfügbare Bildungsgänge an dieser Schule (Grobübersicht):
-        <ul>
-            <li>Berufsschulen »: Wirtschaft, Informatik, Gesundheit</li>
-            <li>Berufsfachschulen »: Wirtschaft, Informatik</li>
-            <li>Fachoberschulen »: Gesundheit, Informatik</li>
-            <li>Berufliches Gymnasium »: Wirtschaft</li>
-            <li>Berufsvorbereitungsjahr »: Wirtschaft</li>
-            <li>Berufsvorbereitungsjahr – Sprachlernklassen »</li>
-        </ul>   
-    </aside>
-    <?php include 'feedback.php';?>
-=======
 <article class>
     <h3 class="card-header"> BBS Wechloy</h3>
     <section class="detailcontainer">
@@ -57,5 +24,4 @@
              <?php include 'feedback.php';?>
         </div>
     </section>
->>>>>>> css-grid-layout
 </article>
