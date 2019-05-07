08777c71b913eaf27d3120902f1da43145ede493
Tim Sigl <tim.sigl@hotmail.de>
2019-05-07 22:46:08 +0200

    slider fix

--- index.php
+++ index.php
@@ -15,13 +15,11 @@
           integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
 </head>
 <body>
-<div id="indexheader">
     <?php include 'pages/header.php'; ?>
-</div>
-<div class="main-container">
     <div class="stretch-grid-item">
         <?php include 'pages/imageslider.php'; ?>
     </div>
+    <div class="main-container"> 
     <div class="large-grid-item card">
         <?php include 'pages/search.php'; ?>
     </div>
--- pages/header.php
+++ pages/header.php
@@ -1,14 +1,14 @@
 <nav class="nav-container">
     <a href="/" class="logo"><h1 >Oscolia</h1></a>
-    <div class = "header">
+    <div class="nav-items">
         <label for="toggle">&#9776;</label>
         <input type="checkbox" id="toggle"/>
-    <div class="menu">
-        <a href ="/index.php#suche" id="searchSchool">Schule suchen</a>
-        <a href ="/index.php#map">Schulen Karten</a>
-        <a href ="/index.php#anlegen">Schule anlegen</a>
-        <a href ="/index.php#anmelden">Anmelden</a>
-        <a href ="/index.php#registrieren">Registrieren</a>
-    </div>
+        <div class="menu">
+            <a href ="/index.php#suche">Schule suchen</a>
+            <a href ="/index.php#map">Schulen Karten</a>
+            <a href ="/index.php#anlegen">Schule anlegen</a>
+            <a href ="/index.php#anmelden">Anmelden</a>
+            <a href ="/index.php#registrieren">Registrieren</a>
+        </div>
     </div>
 </nav>
