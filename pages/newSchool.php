12b852f184281cbd93d19d743330ed9a8f3258ad
Nelly Klassen <nelly.klassen@uni-oldenburg.de>
2019-05-07 17:30:56 +0200

    für cedric

--- pages/newSchool.php
+++ pages/newSchool.php
@@ -1,41 +1,44 @@
 <section>
-    <h2 id="anlegen" class="card-header">Anlegen</h2>
+    <h2 id="anlegen" class="card-header">Neue Schule anlegen</h2>
     <div class="card-body">
-        <form>
-            <input class="input" type="text" name="schulname" placeholder="Name der Schule">
-            <select class="input">
-                <option value="form1">Grundschule</option>
-                <option value="form2">Gymnasium</option>
-                <option value="form3">Oberschule</option>
-                <option value="form4">Förderschule</option>
-                <option value="form5">Integrierte Gesamtschule</option>
-                <option value="form6">Berufsbildende Schule</option>
-            </select>
-            <select class="input">
-                <option value="st1">Alexandersfeld</option>
-                <option value="st2">Bloherfelde</option>
-                <option value="st3">Bürgerfelde</option>
-                <option value="st4">Donnerschwee</option>
-                <option value="st5">Etzhorn</option>
-                <option value="st6">Eversten</option>
-                <option value="st7">Gemeinde Bad Zwischenahn</option>
-                <option value="st8">Innenstadt</option>
-                <option value="st9">Kreyenbrück</option>
-                <option value="st10">Krusenbusch</option>
-                <option value="st11">Nadorst</option>
-                <option value="st12">Neuenwege</option>
-                <option value="st13">Ofenerdiek</option>
-                <option value="st14">Ohmstede</option>
-                <option value="st15">Osternburg</option>
-                <option value="st16">Tweelbäke</option>
-                <option value="st17">Wechloy</option>
-            </select><br>
-            <textarea placeholder="Beschreibung"> </textarea>
-            <br>
-            <div class="card-footer">
-                <input type="file" accept="image/png, image/jpeg">
-                <button class="default-button" type="submit" name="anlegen">Schule anlegen</button>
+        <form class="newSchool-container">
+            <div class="infoHeader-newSchool"> 
+                <input class="input" type="text" id="schoolname" name="schulname" placeholder="Name der Schule">
+                <select class="input" id="schooltype">
+                    <option value="form1">Grundschule</option>
+                    <option value="form2">Gymnasium</option>
+                    <option value="form3">Oberschule</option>
+                    <option value="form4">Förderschule</option>
+                    <option value="form5">Integrierte Gesamtschule</option>
+                    <option value="form6">Berufsbildende Schule</option>
+                </select>
+                <select class="input" id="district">
+                    <option value="st1">Alexandersfeld</option>
+                    <option value="st2">Bloherfelde</option>
+                    <option value="st3">Bürgerfelde</option>
+                    <option value="st4">Donnerschwee</option>
+                    <option value="st5">Etzhorn</option>
+                    <option value="st6">Eversten</option>
+                    <option value="st7">Gemeinde Bad Zwischenahn</option>
+                    <option value="st8">Innenstadt</option>
+                    <option value="st9">Kreyenbrück</option>
+                    <option value="st10">Krusenbusch</option>
+                    <option value="st11">Nadorst</option>
+                    <option value="st12">Neuenwege</option>
+                    <option value="st13">Ofenerdiek</option>
+                    <option value="st14">Ohmstede</option>
+                    <option value="st15">Osternburg</option>
+                    <option value="st16">Tweelbäke</option>
+                    <option value="st17">Wechloy</option>
+                </select>
             </div>
-        </form>
+            <textarea class="textarea" placeholder="Beschreibung"> </textarea>
+
+            <div class="file-upload" data-text="Select your file!">
+                    <input class="file-upload-field" type="file">
+            </div>
+            </form>
+            <br>
+       
     </div>
 </section>
