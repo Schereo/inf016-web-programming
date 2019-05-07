9b762b2ea525fc8333b9910b1d47e2ca979861ad
Cedric Schomaker <cedric.schomaker@uni-oldenburg.de>
2019-05-07 14:42:47 +0200

    feat(css): Layout angepasst, Registrieren; Login

--- pages/login.php
+++ pages/login.php
@@ -1,13 +1,9 @@
 <section>
     <h2 id="anmelden" class="card-header">Anmelden</h2>
-    <form>
-        <div class="card-body">
-            <input class="input login-body" type="text" name="mail" placeholder="E-Mail Adresse"><br>
-            <input class="input login-body" type="password" name="passwort" placeholder="Passwort"><br>
-        </div>
-        <div class="card-footer">
-            <button class="default-button card-footer button-size login-body" type="submit" name="anmelden">Anmelden</button>
-            <a>Passwort vergessen</a>
-        </div>
+    <form class="login-body">
+        <input class="input" id="login-e-mail" type="text" name="mail" placeholder="E-Mail Adresse">
+        <input class="input" id="login-password" type="password" name="passwort" placeholder="Passwort">
+        <button class="default-button card-footer button-size" type="submit" name="anmelden">Anmelden</button>
+        <a href="#">Passwort vergessen</a>
     </form>
 </section>
