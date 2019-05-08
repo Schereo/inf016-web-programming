c1a5d48d37d2e97df4dc843423676b1f94afd199
Tim Sigl <tim.sigl@hotmail.de>
2019-05-08 09:22:46 +0200

    added mobile css file

--- index.php
+++ index.php
@@ -9,7 +9,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="robots" content="noindex">
     <link rel="shortcut icon" type="image/x-icon" href="/assets/favicon.ico"/>
-    <link rel="stylesheet" href="/css/styles.css"/>
+    <link rel="stylesheet" href="/css/desktop/styles.css"/>
+    <link rel="stylesheet" media="(max-width: 768px)" href="/css/mobile/styles-mobile.css"/>
     <link href="https://fonts.googleapis.com/css?family=Noto+Sans" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
           integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
