841212754444705d14fda7b18774474297869fff
Nelly Klassen <nelly.klassen@uni-oldenburg.de>
2019-05-15 23:44:07 +0200

    kommentar feld eingefügt

--- pages/feedback.php
+++ pages/feedback.php
@@ -1,10 +1,10 @@
-<div class="subtitle"> Diese Schule bewerten
-    <form class="rating">
+ <form class="rating">
+        <p>Diese Schule bewerten </p>
         <span class="star" data-value="5">&#9734;</span>
         <span class="star" data-value="4">&#9734;</span>
         <span class="star" data-value="3">&#9734;</span>
         <span class="star" data-value="2">&#9734;</span>
         <span class="star" data-value="1">&#9734;</span> <br>
-        <button type="submit" class="default-button">Bewertung speichern</button>
+        <textarea class="comment" placeholder="Beschreibung"> </textarea> <br>
+     <button type="submit" class="default-button">Bewertung speichern</button>
     </form>
-</div>
