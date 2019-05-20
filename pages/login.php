74398a861dc1b2d59c5107d6756880ee352f1fde
Jens Schulte <jens.schulte@uni-oldenburg.de>
2019-05-20 14:27:59 +0200

    Session eindeutiger gestaltet

--- pages/login.php
+++ pages/login.php
@@ -1,32 +1,4 @@
-<?php
-if (file_exists("creds.json") &&
-    is_readable("creds.json")) {
-    $myfile = file_get_contents("creds.json") or die("Unable to open file!");
-    $credentials = json_decode($myfile);
-}
-if (isset($_POST['anmelden'])) {
-    $found = 0;
-    $usernameInput = strip_tags($_POST['mail']);
-    $passwordInput = strip_tags($_POST['passwort']);
-
-    foreach ($credentials->users as $users) {
-        if ($users->mail == $usernameInput) {
-            $found = 1;
-            if ($users->password == $passwordInput) {
-                $_SESSION['userSessions'] = array(
-                    'username' => $users->mail,
-                    'firstName' => $users->vorname,
-                    'lastName' => $users->nachname,
-                    'login' => 'login');
-                $found = 2;
-            }
-        }
-    }
-    if($found == 0){ print_r("Benutzer nicht gefunden");}
-    if($found == 1){ print_r ("Passwort falsch!");}
-    if($found == 2){ print_r("Login erfolgreich");}
-}
-?>
+<?php require_once('pages/userHandler/loginHandler.php')?>
 <section>
     <h2 id="anmelden" class="card-header">Anmelden</h2>
     <form class="login-body" method="post">
--- pages/register.php
+++ pages/register.php
@@ -1,38 +1,4 @@
-<?php
-$credentialFile = "creds.json";
-if (file_exists($credentialFile) &&
-    is_readable($credentialFile)) {
-    $myfile = file_get_contents("creds.json") or die("Unable to open file!");
-    $credentials = json_decode($myfile);
-
-}
-if (isset($_POST['registrieren'])) {
-    $alreadysaved = false;
-    $forename = strip_tags($_POST['firstName']);
-    $surname = strip_tags($_POST['lastName']);
-    $mailInput = strip_tags($_POST['email']);
-    $passwordInput = strip_tags($_POST['passwort']);
-    $passwordMatch = strip_tags($_POST['passwort2']);
-
-    foreach ($credentials->users as $users) {
-
-        if ($mailInput == $users->mail) {
-            $alreadysaved = true;
-            break;
-        }
-    }
-    if ($passwordInput == $passwordMatch) {
-        $newUserArray = array('vorname' => $forename, 'nachname' => $surname, 'mail' => $mailInput, 'password' => $passwordInput);
-        $credentials->users[] = $newUserArray;
-        file_put_contents($credentialFile, json_encode($credentials));
-        print_r("Registrierung erfolgreich");
-    }
-
-    if ($alreadysaved) {
-        print_r("Benutzer bereits vorhanden.");
-    }
-}
-?>
+<?php require_once('pages/userHandler/registerHandler.php')?>
 <section>
     <h2 id=registrieren class="card-header">Registrieren</h2>
     <form class="register-body" method="post">
