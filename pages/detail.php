7ea09afd2aa9103b1b8198978ad77e84c6f9511b
Cedric Schomaker <cedric.schomaker@uni-oldenburg.de>
2019-05-22 04:01:36 +0200

    merge vorbereitet

--- pages/detail.php
+++ pages/detail.php
@@ -3,72 +3,77 @@
     require_once 'php-business/schoolDao.php';
     require_once 'php-business/schoolJson.php';
     require_once 'php-business/schoolView.php';
-    if(isset($schoolio)):
+    if (isset($schoolio)):
     ?>
 
-    <h2 class="card-header"> <?= $schoolio->name?>
-    <img src="assets/edit.png">  </h2>
-    <form class="detailcontainer card-body" method="GET" action="<?php echo $_SERVER['PHP_SELF']?>">
-        <img src="../assets/images/<?= $schoolio->imagePath?>" alt="Picture of BBS Wechloy" class="responsive detail-picture">
+    <h2 class="card-header"> <?= $schoolio->name ?>
+        <form method="post">
+            <button name="edit">
+            <img src="/assets/edit.png" alt="Bearbeiten">
+            </button>
+        </form>
+    </h2>
+    <form class="detailcontainer card-body" method="GET" action="<?php echo $_SERVER['PHP_SELF'] ?>">
+        <img src="../assets/images/hgo1.jpg" alt="Picture of BBS Wechloy" class="responsive detail-picture">
         <div class="detail-contact">
 
-                <h1 class="info-container detail-headline">
-                    Berufsbildungszentrum für Wirtschaft, Informatik und Gesundheit
-                </h1>
-                <div class="info-container">
-                    <div class="text-left">
-                        Schulleitung:
-                    </div>
-                    <div class="text-right">
-                        <?= $schoolio->principal ?>
-                    </div>
+            <h1 class="info-container detail-headline">
+                Berufsbildungszentrum für Wirtschaft, Informatik und Gesundheit
+            </h1>
+            <div class="info-container">
+                <div class="text-left">
+                    Schulleitung:
                 </div>
-                <div class="info-container">
-                    <div class="text-left">
-                        Adresse:
-                    </div>
-                    <div class="text-right">
-                       <?=$schoolio->address->street?><br> <?=$schoolio->address->zipCode?> <?=$schoolio->address->city?>                    </div>
+                <div class="text-right">
+                    <?= $schoolio->principal ?>
                 </div>
-                <div class="info-container">
-                    <div class="text-left">
-                        Telefon:
-                    </div>
-                    <div class="text-right">
-                        <a href="tel:<?=$schoolio->phoneNumber?>"><?=$schoolio->phoneNumber?></a>
-                    </div>
+            </div>
+            <div class="info-container">
+                <div class="text-left">
+                    Adresse:
                 </div>
-                <div class="info-container">
-                    <div class="text-left">
-                        E-Mail:
-                    </div>
-                    <div class="text-right">
-                        <a href="mailto:<?=$schoolio->mail?>"><?=$schoolio->mail?></a>
-                    </div>
+                <div class="text-right">
+                    <?= $schoolio->address->street ?>
+                    <br> <?= $schoolio->address->zipCode ?> <?= $schoolio->address->city ?>                    </div>
+            </div>
+            <div class="info-container">
+                <div class="text-left">
+                    Telefon:
                 </div>
-                <div class="info-container">
-                    <div class="text-left">
-                        Internet:
-                    </div>
-                    <div class="text-right">
-                        <a href="<?=$schoolio->homepageUrl?>"><?=$schoolio->homepageUrl?></a>
-                    </div>
+                <div class="text-right">
+                    <a href="tel:<?= $schoolio->phoneNumber ?>"><?= $schoolio->phoneNumber ?></a>
                 </div>
-                <div class="detail-info">
-                    <h5 class="detail-headline">Weitere Infos</h5>
-                    <div>
-                        <?= $schoolio->description?>
-                    </div>
+            </div>
+            <div class="info-container">
+                <div class="text-left">
+                    E-Mail:
                 </div>
-            </section>
+                <div class="text-right">
+                    <a href="mailto:<?= $schoolio->mail ?>"><?= $schoolio->mail ?></a>
+                </div>
+            </div>
+            <div class="info-container">
+                <div class="text-left">
+                    Internet:
+                </div>
+                <div class="text-right">
+                    <a href="<?= $schoolio->homepageUrl ?>"><?= $schoolio->homepageUrl ?></a>
+                </div>
+            </div>
+            <div class="detail-info">
+                <h5 class="detail-headline">Weitere Infos</h5>
+                <div>
+                    <?= $schoolio->description ?>
+                </div>
+            </div>
         </div>
         <?php if (isset($_SESSION['userSessions'])) { ?>
         <div class="detail-feedback">
             <?php include 'feedback.php'; ?>
         </div>
     </form>
-            <div class="text-rigt"><a href="">
-                </a></div>  <?php } ?>
-    </section>
+    <div class="text-right">
+        <a href=""></a>
+    </div> <?php } ?>
 </article>
 <?php endif ?>
