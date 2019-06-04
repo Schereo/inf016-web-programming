feb633a04774312da6e8cf2dcc828e00d156c9eb
Nelly Klassen <nelly.klassen@uni-oldenburg.de>
2019-06-04 19:19:37 +0200

    decode statt encode...

--- pages/editSchool/displayUploads.php
+++ pages/editSchool/displayUploads.php
@@ -2,4 +2,4 @@
 
 $query = new Query((new DatabaseConnector())->connect());
 $img = $query->getUploadedImages();
-echo '<img src="data:image/png;base64,' . base64_encode($img['data']) . '"/>';
+echo '<img src="data:image/png;base64,' . base64_decode($img['data']) . '"/>';
