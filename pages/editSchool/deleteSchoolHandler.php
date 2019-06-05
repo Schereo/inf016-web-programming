e05dd4e418a55c0c032322e4870daf27c8b81434
Nelly Klassen <nelly.klassen@uni-oldenburg.de>
2019-06-05 10:10:34 +0200

    delete images, schools

---
+++ pages/editSchool/deleteSchoolHandler.php
@@ -0,0 +1,7 @@
+<?php
+session_start();
+require_once "../../database/Delete.php";
+
+    $delete = new Delete((new DatabaseConnector())->connect());
+    $delete->school($_POST['deleteID']);
+    header("Location: ../../index.php");
--- pages/editSchool/displayUploads.php
+++ pages/editSchool/displayUploads.php
@@ -1,11 +1,10 @@
-<?php require_once "database/Delete.php";
+<?php
 
 $query = new Query((new DatabaseConnector())->connect());
-$delete = new Delete((new DatabaseConnector())->connect());
 
 $img = $query->getUploadedImages($_SESSION['user_ID']);
 
 foreach($img as $image) {
-    echo ' <form action="" class="uploadedPictures"><img src="data:image/png;base64,' . base64_encode($image['data']) . '"/> <br> 
- <button class="delete" type="submit"> löschen </button> </form>' ;
+    echo '<form action="pages/editSchool/deleteImgHandler.php" method="post" class="uploadedPictures"><img src="data:image/png;base64,' . base64_encode($image['data']) . '"/> <br> 
+ <button class="delete" type="submit" name="delete_img" value="'.$image['image_id'].'"/> löschen </button> </form>' ;
 }
