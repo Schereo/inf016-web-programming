4bb504e46a46812bcbb821622e51abeb88d2a750
Jens Schulte <jens.schulte@uni-oldenburg.de>
2019-06-14 21:32:03 +0200

    Fix damit das richtig funktioniert mit und ohne js

--- pages/register/register.php
+++ pages/register/register.php
@@ -6,9 +6,16 @@
         <input class="input" type="text" name="firstNameReg" placeholder="Vorname">
         <input class="input" type="text" name="lastNameReg" placeholder="Nachname">
         <input class="input" type="email" name="emailReg" placeholder="Email">
-        <input class="input"  id="regPassword" type="password" name="password" placeholder="Passwort" onload="checkPasswordStrength()">
-        <script><meter max="4" id="password-strength-meter"></meter>
-        <p id="password-strength-text"></p></script>
+        <input class="input"  id="regPassword" type="password" name="password" placeholder="Passwort">
+
+        <meter max="4" id="password-strength-meter"></meter>
+        <p id="password-strength-text"></p>
+        <!-- Enable the CheckPasswordStrength + Bar and Info when Scripts are enabled, default CSS property : display:hidden-->
+        <script onload="checkPasswordStrength()">
+        document.getElementById('password-strength-meter').style.display='block';
+        document.getElementById('password-strength-text').style.display='block';
+        </script>
+
         <input class="input" type="password" name="password2Reg" placeholder="Passwort wiederholen">
         <button class="default-button button-size" type="submit" name="registrieren"> Registrieren</button>
     </form>
