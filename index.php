21f483dc9ce7be8efba6920c3123b62a13e81ee4
Jens Schulte <jens.schulte@uni-oldenburg.de>
2019-06-16 22:58:29 +0200

    CSS umgeordnet damit unser styles.css das von Bootstrap überschreibt.

--- index.php
+++ index.php
@@ -39,10 +39,10 @@ if (isset($_POST['type'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="robots" content="noindex">
     <link rel="shortcut icon" type="image/x-icon" href="./assets/favicon.ico"/>
-    <link rel="stylesheet" href="css/styles.css"/>
     <link href="https://fonts.googleapis.com/css?family=Noto+Sans" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/styles.css"/>
     <script src="js/userFunctions.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
