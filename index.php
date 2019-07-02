a18f06d100d83a42a60620b6548ab49b6ddc9dc6
Tim Sigl <tim.sigl@hotmail.de>
2019-07-03 01:24:54 +0200

    Cookies akzeptieren Banner hinzugefügt. JS für AJAX session starten. Anzeigen und verstecken Login Registrieren je nach dem, ob Cookies enabled / disabled sind.

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
-<?php session_start();
+<?php if (isset($_COOKIE['acceptedCookies'])) {
+    session_start();
+}
 error_reporting(0);
 $depth = "";
 require_once 'pages/login/loginHandler.php';
@@ -42,6 +44,7 @@ if (isset($_POST['type'])&& isset($_POST['checkbox'])) {
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <script src="js/upload.js"></script>
 
+
 </head>
 <body>
 <?php
@@ -82,6 +85,10 @@ include 'imageslider.php';
         </div>
     <?php } ?>
 </div>
+<div class="alert text-center" id="cookie-banner">
+    Unsere Website benutzt Cookies &#x1F36A;. Um die Website komplett benutzen zu können, müssen Sie diesen zustimmen.
+    <button class="btn default-button cookie-accept">Akzeptieren</button>
+</div>
 <div class="stretch-grid-item" id="indexfooter">
     <?php include $depth . 'footer.php'; ?>
 </div>
@@ -101,5 +108,6 @@ include 'imageslider.php';
         document.getElementById("anmelden").scrollIntoView();
     }
 </script>
+<script src="js/cookieBanner.js"></script>
 </body>
 </html>
--- pages/login/login.php
+++ pages/login/login.php
@@ -1,13 +1,9 @@
-<section>
+<section id="anmeldenSection">
     <h2 id="anmelden" class="card-header">Anmelden</h2>
     <form class="login-body" method="post">
         <input type="hidden" name="type" value="Login">
         <input class="input" id="login-e-mail" type="email" name="emailLogin" placeholder="E-Mail Adresse" required>
         <input class="input" id="login-password" type="password" name="passwordLogin" placeholder="Passwort" required>
-        <div>
-            Mit dem Login bin ich damit einverstanden, dass die Seite Cookies speichert.
-        <input type="checkbox" name="checkbox" class="checkbox" value="check" id="agree" />
-        </div>
         <?php
         if (isset($loginError) && $loginError === 'loginFehler') { ?>
             <div class="errorLabel" style="color: red; font-size: 15px">
